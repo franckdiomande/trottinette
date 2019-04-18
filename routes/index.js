aed8cdb59b4edfadc7563b1e857008118b5a5c60
julien93140 <julien.moratalla@gmail.com>
2019-04-18 20:14:55 +0200

    chart in process

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
+const path = require('path');
+
+const dir = '../views/chart.html';
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Trottinette' });
+});
+
+
+router.get('/chart', function(req, res, next) {
+  res.sendFile(path.join(__dirname, '../views', 'chart.html'));
 });
 
 module.exports = router;
